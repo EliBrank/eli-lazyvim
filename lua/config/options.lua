6ecae15f8ea255e8263c546e0dca40c1561917bc
EliBrank <eliasbrinkman@gmail.com>
2024-07-18 21:22:46 +0000

    change expandtab back to true to change to spaces instead of tabs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
-vim.opt.expandtab = false -- Pressing the TAB key will insert actual tab characters
+vim.opt.expandtab = true -- Pressing the TAB key will insert actual tab characters
 vim.opt.tabstop = 4 -- Number of spaces tabs count for
 vim.g.autoformat = false -- Globally prevent autoformat on save
